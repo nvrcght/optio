7a6796d92e28f7d50e14956cde6611e949238bff
nvrcght <nvrcght@gmail.com>
2015-09-19 11:57:34 -0700

    Initial Commit

---
+++ optio/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  optio
+//
+//  Created by Anush Pogosyan on 9/19/15.
+//  Copyright (c) 2015 Anush Pogosyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
